9eb2c7787e602819362b01d6d3a24131b3ff8641
OndrejHana <58571966+OndrejHana@users.noreply.github.com>
2022-11-13 14:40:18 +0100

    I still dunno what im doing

--- index.ts
+++ index.ts
@@ -7,14 +7,19 @@ import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
 
 const { PORT } = config();
 
-const app: Application = new Application();
 const router: Router = new Router();
-
 router.get("/", root_handler);
 router.get("/message", get_messages);
 router.post("/message", post_message);
 
+const app: Application = new Application();
 app.use(log_request);
 app.use(router.routes());
 app.use(router.allowedMethods());
 
+app.addEventListener(
+  "listen",
+  (e) => console.log("Listening on http://localhost:8080"),
+);
+
+await app.listen({ port: PORT});
